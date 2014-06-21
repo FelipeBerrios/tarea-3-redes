16d69d416566dc2f41a6f1cf09e0480a91695e66
FelipeBerrios <felipe.berriost@alumnos.usm.cl>
2014-06-20 22:15:48 -0400

    Se añaden las funciones de la pregunta 3
    Se termino la pregunta 3 y se genero el archivo de texto.

---
+++ Readme.txt
@@ -0,0 +1,12 @@
+En la tarea se utilizó la librería numpy de Python, para trabajar más fácilmente los arreglos.
+Para la pregunta 2 el programa muestra el resultado por consola y también en un archivo,
+Lo que se aprecia son las tablas de cada router, como cambian estas en cada iteración del
+Algoritmo, se debe considerar la notación "inf" como infinito, ya que así parte la matriz antes de
+asignar sus vecinos.
+Para la pregunta 3 se recalcularon los costes si se cortaba el enlace de H e I y se generó el archivo
+tablas_P3.txt , que contienen los costes asociados si el enlace nombrado anteriormente no existiera.
+
+
+
+Felipe Berrios Toloza 201173586-2
+Felipe Araya Barrera 201173501-3
--- tarea3.py
+++ tarea3.py
@@ -1,8 +1,15 @@
 from numpy import *
 
+#Se definen las variables para no confundirse al trabajar con indices de las matrices
+
 A,B,C,D,E,F,G,H,I= 0,1,2,3,4,5,6,7,8
 
+#Se crearan 2 archivos para mostrar las matrices
 miarchivo=open("tablas.txt","w")
+miarchivo3=open("tablas_P3.txt","w")
+
+#Esta funcion Obtiene los vecinos de la matriz pasado, y retorna en una lista
+#Se hace basicamente para la salida en el archivo y el formato de las tablas.
 def Obtenervecinos(X):
 	listavecinos=[]
 	for i in range(0,len(vecinos)):
@@ -53,6 +60,8 @@ def Obtenervecinos(X):
 
 	return listavecinos
 
+#Asocia una tabla con su respectivo Indice, devuelve la tabla original
+
 def AsociarTabla(i):
 	if i==0:
 		return costosA
@@ -75,7 +84,10 @@ def AsociarTabla(i):
 	else:
 		return "error"
 
-def ImprimirTablas(lista):
+
+
+#Funcion para imprimir una lista con las tablas acumuladas, con el formato pedido.
+def ImprimirTablas(lista,miarchivo):
 	for x in lista:
 		for i in range(0,len(x)):
 			for j in range(0,len(x[0])):
@@ -83,15 +95,15 @@ def ImprimirTablas(lista):
 				miarchivo.write(str(x[i][j])+"\t|")
 			print
 			miarchivo.write("\n")
-			for j in range(0,len(x[0])):
-				print "---\t|",
-				miarchivo.write("---\t|")
-			print
+			#for j in range(0,len(x[0])):
+			sep="-"*77
+			print sep
+			miarchivo.write(sep)
 			miarchivo.write("\n")
 		print "\n"
-		miarchivo.write("\n")
+		miarchivo.write("\n\n")
 
-
+#Formatea la tabla para dejar las letras de los routers en la primera fila y primera columna.
 def FormatearTabla(tabla,X):
 	listatotal=["/","A","B","C","D","E","F","G","H","I"]
 	tabla2=array(tabla)
@@ -115,7 +127,7 @@ def FormatearTabla(tabla,X):
 
 	
 
-
+#Cuando un vector se actualiza, es enviado a sus vecinos
 def EnviarVector(X):
 	tabla=AsociarTabla(X)
 	for i in range(0,tabla.shape[0]):
@@ -123,6 +135,8 @@ def EnviarVector(X):
 			tabla2=AsociarTabla(i)
 			tabla2[X]=tabla[X]
 
+
+#Asigan los vecinos de un Indice a cierta tabla, para solo trabajar con estos.
 def AsignarVecinos(X):
 	tabla=AsociarTabla(X)
 	for i in range(0,costos.shape[0]):
@@ -130,13 +144,14 @@ def AsignarVecinos(X):
 			tabla2=AsociarTabla(i)
 			tabla[i]=tabla2[i]
 
-
+#Tabla inicial a partir de la matriz de costos
 def CrearTablaNodo(X):
 	mitabla=zeros((9,9))
 	mitabla=mitabla+float("inf")
 	mitabla[X]=costos[X]
 	return mitabla
 
+#Se realiza el algoritmo de BellmandFord senialado en las diapos.
 def BellmanFord(D):
 	destino=AsociarTabla(D)
 	for i in range(0,destino.shape[1]):
@@ -150,13 +165,14 @@ def BellmanFord(D):
 
 
 
-
+#Funcion que comprueba mediante la tabla vecinos, si 2 Routers son vecinos
 
 def EsVecino(X,Y):
 	if(vecinos[X][Y]==1):
 		return True
 	else:
 		return False
+#En primer Luegar se crean y rellena las tablas vecinos y costos.
  
 vecinos= zeros((9,9))
 costos= zeros((9,9))
@@ -267,6 +283,8 @@ costosG= CrearTablaNodo(G)
 costosH= CrearTablaNodo(H)
 costosI= CrearTablaNodo(I)
 
+#En las listas de cada router se muestra la progresion de cada tabla
+
 ListaA.append(FormatearTabla(array(costosA),A))
 ListaB.append(FormatearTabla(array(costosB),B))
 ListaC.append(FormatearTabla(array(costosC),C))
@@ -277,7 +295,7 @@ ListaG.append(FormatearTabla(array(costosG),G))
 ListaH.append(FormatearTabla(array(costosH),H))
 ListaI.append(FormatearTabla(array(costosI),I))
 
-
+#costo anterior maneja la matriz de costos antes de aplicar bellmanford, para ver si cambio en algo su vector director.
 costosAanterior= array(costosA)
 costosBanterior= array(costosB)
 costosCanterior= array(costosC)
@@ -287,6 +305,7 @@ costosFanterior= array(costosF)
 costosGanterior= array(costosG)
 costosHanterior= array(costosH)
 costosIanterior= array(costosI)
+
 
 AsignarVecinos(A)
 AsignarVecinos(B)
@@ -308,7 +327,199 @@ ListaG.append(FormatearTabla(array(costosG),G))
 ListaH.append(FormatearTabla(array(costosH),H))
 ListaI.append(FormatearTabla(array(costosI),I))
 
+#se itera consecutivamente hasta que todas las matrices no cambien.
+
 cambio=9
+
+
+while cambio>0:
+	cambio=0
+	#si son distintas, quiere decir que un vector cambio y se debe recomputar con BF.
+	if not array_equal(costosAanterior,costosA):
+		costosAanterior=array(costosA)
+		BellmanFord(A)
+		cambio+=1
+	if not array_equal(costosBanterior,costosB):
+		costosBanterior=array(costosB)
+		BellmanFord(B)
+		cambio+=1
+	if not array_equal(costosCanterior,costosC):
+		costosCanterior=array(costosC)
+		BellmanFord(C)
+		cambio+=1
+	if not array_equal(costosDanterior,costosD):
+		costosDanterior=array(costosD)
+		BellmanFord(D)
+		cambio+=1
+	if not array_equal(costosEanterior,costosE):
+		costosEanterior=array(costosE)
+		BellmanFord(E)
+		cambio+=1
+	if not array_equal(costosFanterior,costosF):
+		costosFanterior=array(costosF)
+		BellmanFord(F)
+		cambio+=1
+	if not array_equal(costosGanterior,costosG):
+		costosGanterior=array(costosG)
+		BellmanFord(G)
+		cambio+=1
+	if not array_equal(costosHanterior,costosH):
+		costosHanterior=array(costosH)
+		BellmanFord(H)
+		cambio+=1
+	if not array_equal(costosIanterior,costosI):
+		costosIanterior=array(costosI)
+		BellmanFord(I)
+		cambio+=1
+
+	#Se adieren las tablas a la lista.
+	if cambio!=0:
+		ListaA.append(FormatearTabla(array(costosA),A))
+		ListaB.append(FormatearTabla(array(costosB),B))
+		ListaC.append(FormatearTabla(array(costosC),C))
+		ListaD.append(FormatearTabla(array(costosD),D))
+		ListaE.append(FormatearTabla(array(costosE),E))
+		ListaF.append(FormatearTabla(array(costosF),F))
+		ListaG.append(FormatearTabla(array(costosG),G))
+		ListaH.append(FormatearTabla(array(costosH),H))
+		ListaI.append(FormatearTabla(array(costosI),I))
+
+	#Si cambia e vector director de esa matriz, se debe indicar a los vecinos para que ellos recomputen su propio vector director
+
+	if not array_equal(costosAanterior[A],costosA[A]):
+		costosAanterior[A]=costosA[A]
+		EnviarVector(A)
+
+	if not array_equal(costosBanterior[B],costosB[B]):
+		costosBanterior[B]=costosB[B]
+		EnviarVector(B)
+
+	if not array_equal(costosCanterior[C],costosC[C]):
+		costosCanterior[C]=costosC[C]
+		EnviarVector(C)
+
+	if not array_equal(costosDanterior[D],costosD[D]):
+		costosDanterior[D]=costosD[D]
+		EnviarVector(D)
+
+	if not array_equal(costosEanterior[E],costosE[E]):
+		costosEanterior[E]=costosE[E]
+		EnviarVector(E)
+
+	if not array_equal(costosFanterior[F],costosF[F]):
+		costosFanterior[F]=costosF[F]
+		EnviarVector(F)
+
+	if not array_equal(costosGanterior[G],costosG[G]):
+		costosGanterior[G]=costosG[G]
+		EnviarVector(G)
+
+	if not array_equal(costosHanterior[H],costosH[H]):
+		costosHanterior[H]=costosH[H]
+		EnviarVector(H)
+
+	if not array_equal(costosIanterior[I],costosI[I]):
+		costosIanterior[I]=costosI[I]
+		EnviarVector(I)
+
+#Se imprime en Consola y en un archivo las respectivas matrices
+print "Tabla Router A:\n"
+miarchivo.write("Tabla Router A:\n\n")
+ImprimirTablas(ListaA,miarchivo)
+
+print "Tabla Router B:\n"
+miarchivo.write("Tabla Router B:\n\n")
+ImprimirTablas(ListaB,miarchivo)
+
+print "Tabla Router C:\n"
+miarchivo.write("Tabla Router C:\n\n")
+ImprimirTablas(ListaC,miarchivo)
+
+print "Tabla Router D:\n"
+miarchivo.write("Tabla Router D:\n\n")
+ImprimirTablas(ListaD,miarchivo)
+
+print "Tabla Router E:\n"
+miarchivo.write("Tabla Router E:\n\n")
+ImprimirTablas(ListaE,miarchivo)
+
+print "Tabla Router F:\n"
+miarchivo.write("Tabla Router F:\n\n")
+ImprimirTablas(ListaF,miarchivo)
+
+print "Tabla Router G:\n"
+miarchivo.write("Tabla Router G:\n\n")
+ImprimirTablas(ListaG,miarchivo)
+
+print "Tabla Router H:\n"
+miarchivo.write("Tabla Router H:\n\n")
+ImprimirTablas(ListaH,miarchivo)
+
+print "Tabla Router I:\n"
+miarchivo.write("Tabla Router I:\n\n")
+ImprimirTablas(ListaI,miarchivo)
+
+
+
+#Pregunta 3
+#Aqui basicamente se pide recalcular los costos si desaparece el enlace entre H e I
+
+#Ya no son vecinos
+vecinos[H][I]=0
+vecinos[I][H]=0
+
+#No hay costo asociado
+costos[H][I]=float("inf")
+costos[I][H]=float("inf")
+
+costosA= CrearTablaNodo(A)
+costosB= CrearTablaNodo(B)
+costosC= CrearTablaNodo(C)
+costosD= CrearTablaNodo(D)
+costosE= CrearTablaNodo(E)
+costosF= CrearTablaNodo(F)
+costosG= CrearTablaNodo(G)
+costosH= CrearTablaNodo(H)
+costosI= CrearTablaNodo(I)
+
+#Se realiza un proceso similar al anterior
+costosAanterior= array(costosA)
+costosBanterior= array(costosB)
+costosCanterior= array(costosC)
+costosDanterior= array(costosD)
+costosEanterior= array(costosE)
+costosFanterior= array(costosF)
+costosGanterior= array(costosG)
+costosHanterior= array(costosH)
+costosIanterior= array(costosI)
+
+
+
+
+AsignarVecinos(A)
+AsignarVecinos(B)
+AsignarVecinos(C)
+AsignarVecinos(D)
+AsignarVecinos(E)
+AsignarVecinos(F)
+AsignarVecinos(G)
+AsignarVecinos(H)
+AsignarVecinos(I)
+
+
+ListaA3=[]
+ListaB3=[]
+ListaC3=[]
+ListaD3=[]
+ListaE3=[]
+ListaF3=[]
+ListaG3=[]
+ListaH3=[]
+ListaI3=[]
+
+
+cambio=9
+
 
 while cambio>0:
 	cambio=0
@@ -350,15 +561,15 @@ while cambio>0:
 		cambio+=1
 
 	if cambio==0:
-		ListaA.append(FormatearTabla(array(costosA),A))
-		ListaB.append(FormatearTabla(array(costosB),B))
-		ListaC.append(FormatearTabla(array(costosC),C))
-		ListaD.append(FormatearTabla(array(costosD),D))
-		ListaE.append(FormatearTabla(array(costosE),E))
-		ListaF.append(FormatearTabla(array(costosF),F))
-		ListaG.append(FormatearTabla(array(costosG),G))
-		ListaH.append(FormatearTabla(array(costosH),H))
-		ListaI.append(FormatearTabla(array(costosI),I))
+		ListaA3.append(FormatearTabla(array(costosA),A))
+		ListaB3.append(FormatearTabla(array(costosB),B))
+		ListaC3.append(FormatearTabla(array(costosC),C))
+		ListaD3.append(FormatearTabla(array(costosD),D))
+		ListaE3.append(FormatearTabla(array(costosE),E))
+		ListaF3.append(FormatearTabla(array(costosF),F))
+		ListaG3.append(FormatearTabla(array(costosG),G))
+		ListaH3.append(FormatearTabla(array(costosH),H))
+		ListaI3.append(FormatearTabla(array(costosI),I))
 
 	if not array_equal(costosAanterior[A],costosA[A]):
 		costosAanterior[A]=costosA[A]
@@ -396,42 +607,45 @@ while cambio>0:
 		costosIanterior[I]=costosI[I]
 		EnviarVector(I)
 
-#listavecinosA=Obtenervecinos(A)
-#ImprimirTablas(ListaA,listavecinosA)	
-print "Router A\n"
-miarchivo.write("Router A\n\n")
-ImprimirTablas(ListaA)
+#Se imprime la salida de la pregunta 3, con sus respectivas tablas de costos para cada Router.
+print "Pregunta 3\n"
 
-print "Router B\n"
-miarchivo.write("Router B\n\n")
-ImprimirTablas(ListaB)
 
-print "Router C\n"
-miarchivo.write("Router C\n\n")
-ImprimirTablas(ListaC)
+print "Tabla Router A:\n"
+miarchivo3.write("Tabla Router A:\n\n")
+ImprimirTablas(ListaA3,miarchivo3)
 
-print "Router D\n"
-miarchivo.write("Router D\n\n")
-ImprimirTablas(ListaD)
+print "Tabla Router B:\n"
+miarchivo3.write("Tabla Router B:\n\n")
+ImprimirTablas(ListaB3,miarchivo3)
 
-print "Router E\n"
-miarchivo.write("Router E\n\n")
-ImprimirTablas(ListaE)
+print "Tabla Router C:\n"
+miarchivo3.write("Tabla Router C:\n\n")
+ImprimirTablas(ListaC3,miarchivo3)
 
-print "Router F\n"
-miarchivo.write("Router F\n\n")
-ImprimirTablas(ListaF)
+print "Tabla Router D:\n"
+miarchivo3.write("Tabla Router D:\n\n")
+ImprimirTablas(ListaD3,miarchivo3)
 
-print "Router G\n"
-miarchivo.write("Router G\n\n")
-ImprimirTablas(ListaG)
+print "Tabla Router E:\n"
+miarchivo3.write("Tabla Router E:\n\n")
+ImprimirTablas(ListaE3,miarchivo3)
 
-print "Router H\n"
-miarchivo.write("Router H\n\n")
-ImprimirTablas(ListaH)
+print "Tabla Router F:\n"
+miarchivo3.write("Tabla Router F:\n\n")
+ImprimirTablas(ListaF3,miarchivo3)
 
-print "Router I\n"
-miarchivo.write("Router I\n\n")
-ImprimirTablas(ListaI)
+print "Tabla Router G:\n"
+miarchivo3.write("Tabla Router G:\n\n")
+ImprimirTablas(ListaG3,miarchivo3)
+
+print "Tabla Router H:\n"
+miarchivo3.write("Tabla Router H:\n\n")
+ImprimirTablas(ListaH3,miarchivo3)
+
+print "Tabla Router I:\n"
+miarchivo3.write("Tabla Router I:\n\n")
+ImprimirTablas(ListaI3,miarchivo3)
 
 miarchivo.close()
+miarchivo3.close()
